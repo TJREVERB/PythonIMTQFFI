a2d6216a10552ff9e9c6786f50f8eb977f55f8c9
max2442 <43480994+max2442@users.noreply.github.com>
2019-03-22 11:47:12 -0400

    Update isisimtq.pyx

--- isisimtq.pyx
+++ isisimtq.pyx
@@ -2,30 +2,30 @@ from libc.stdint cimport uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t,
 from libcpp cimport bool
 cdef extern from "json.h":
   ctypedef struct JsonNode:
-	  /* only if parent is an object or array (NULL otherwise) */
-	  JsonNode *parent;
-	  JsonNode *prev, *next;
+	/* only if parent is an object or array (NULL otherwise) */
+	JsonNode *parent;
+	JsonNode *prev, *next;
 	
-	  /* only if parent is an object (NULL otherwise) */
-	  char *key; /* Must be valid UTF-8. */
+	/* only if parent is an object (NULL otherwise) */
+	char *key; /* Must be valid UTF-8. */
 	
-	  JsonTag tag;
-	  union {
-		  /* JSON_BOOL */
-		  bool bool_;
+	JsonTag tag;
+	union {
+		/* JSON_BOOL */
+		bool bool_;
 		
-		  /* JSON_STRING */
-		  char *string_; /* Must be valid UTF-8. */
+		/* JSON_STRING */
+		char *string_; /* Must be valid UTF-8. */
 		
-		  /* JSON_NUMBER */
-		  double number_;
+		/* JSON_NUMBER */
+		double number_;
 		
-		  /* JSON_ARRAY */
-		  /* JSON_OBJECT */
-		  struct {
-			  JsonNode *head, *tail;
-		  } children;
-	  };
+		/* JSON_ARRAY */
+		/* JSON_OBJECT */
+		struct {
+			JsonNode *head, *tail;
+		} children;
+	};
 	
 cdef extern from "imtq-config.h":
   ctypedef union imtq_config_value:
