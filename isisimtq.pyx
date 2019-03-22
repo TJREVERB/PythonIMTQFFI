2056664c23ccf9b11a3bcefa29b9adcdd2b5cfaf
max2442 <43480994+max2442@users.noreply.github.com>
2019-03-22 11:41:47 -0400

    Update isisimtq.pyx

--- isisimtq.pyx
+++ isisimtq.pyx
@@ -1,5 +1,7 @@
 from libc.stdint cimport uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t, int64_t
 from libcpp cimport bool
+cdef extern from "json.h":
+  ctypedef struct JsonNode JsonNode
 cdef extern from "imtq-config.h":
   ctypedef union imtq_config_value:
       int8_t int8_val;                #*< Storage for signed single-byte values */
