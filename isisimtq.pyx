7facbf9e02b0a8485608020f2326cebd03aa0dab
max2442 <43480994+max2442@users.noreply.github.com>
2019-03-22 11:23:22 -0400

    Update isisimtq.pyx

--- isisimtq.pyx
+++ isisimtq.pyx
@@ -1,4 +1,4 @@
-from libc.stdint cimport uint8_t, uint16_t, uint32_t, int8_t, int16_t, int32_t
+from libc.stdint cimport uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t, int64_t
 from libcpp cimport bool
 cdef extern from "imtq-config.h":
   ctypedef union imtq_config_value:
