e6a853fcc6cf391f7465870764fa1dbaf48325df
max2442 <43480994+max2442@users.noreply.github.com>
2019-03-28 16:16:20 -0400

    Update isisimtq.pyx

--- isisimtq.pyx
+++ isisimtq.pyx
@@ -1,10 +1,10 @@
 from libc.stdint cimport uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t, int64_t
 from libcpp cimport bool
-#cdef extern from "json.h":
-  #ctypedef struct JsonNode:
+cdef extern from "json.h":
+  ctypedef struct JsonNode:
     #/* only if parent is an object or array (NULL otherwise) */
-    #JsonNode *parent;
-    ##JsonNode *prev, *next;
+    JsonNode *parent;
+    JsonNode *prev, *next;
 	
     #/* only if parent is an object (NULL otherwise) */
     #char *key; #/* Must be valid UTF-8. */
